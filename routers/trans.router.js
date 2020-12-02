b99ad0d8cd022f18b18c32d99d9136ca59fb0699
unknown <phongnguyenktb@gmail.com>
2020-12-02 09:59:39 +0700

    search id book - user

--- routers/trans.router.js
+++ routers/trans.router.js
@@ -7,17 +7,41 @@ var router = express.Router();
 // show người đã mượn sách
 router.get("/",(req, res) => {
 	var trans = db.get('trans').value();
+	var users = db.get('user').value();
+	var books = db.get('books').value();
 
-	res.render("trans", {
-    	trans
+	var result = [];
+
+	trans.map(function(item){
+		var obj = {};   
+		obj.id = item.id;
+
+		users.map(function(user){
+			if(item.userId === user.id){
+				return obj.user = user;
+			}
+		});
+
+		books.map(function(book){
+			if(item.bookId === book.id){
+				return obj.book = book;
+			}
+		});
+		return result.push(obj)
 	});
+
+	console.log(result)
+
+	res.render('trans', {
+		transactions: result
+	})
 });
 // thêm userId và BookId mới
 router.get("/create", (req, res) => {
 	var users = db.get('user').value();
 	var books = db.get('books').value();
 
- 	res.render("transadd", {
+ 	res.render("transcreate", {
  		users: users,
  		books: books
  	});
